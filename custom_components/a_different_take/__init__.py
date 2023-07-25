7df800aa183c8726079818fa047d56eba465eab1
Dean Galvin <deangalvin3@gmail.com>
2023-07-24 21:55:29 -0400

    Adds migrate

--- custom_components/a_different_take/__init__.py
+++ custom_components/a_different_take/__init__.py
@@ -13,3 +13,13 @@ async def async_setup_entry(hass, config_entry):
     load_dashboard(hass, config_entry)
 
     return True
+
+async def async_migrate_entry(hass, config_entry: ConfigEntry):
+    """Migrate old entry."""
+    if config_entry.version == 1:
+
+        new = {**config_entry.data}
+
+        config_entry.version = 2
+        hass.config_entries.async_update_entry(config_entry, data=new)
+    return True
